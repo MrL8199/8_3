b35d44737bdae5463977ccb0de88d6e38a771cd9
Đinh Văn Lực <dinhvanluc69@gmail.com>
2021-03-07 12:10:07 +0700

    done

--- js/script.js
+++ js/script.js
@@ -1,25 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	new WOW().init();
 	$('.logo-header-content').addClass('heartBeat animated');
 
 	$('.list-image li a').fancybox();
 
-	$(window).scroll(function(event) {
-		var pos_body = $('html, body').scrollTop(); 
+	$(window).scroll(function (event) {
+		var pos_body = $('html, body').scrollTop();
 		var pos_loveStory = $('.lovestory-main').offset().top;
 
-		if(pos_body>$(window).height()){
+		if (pos_body > $(window).height()) {
 			$('.back-to-top').addClass('show-btn');
 			$('.logo-header').addClass('sticky-top').addClass('tada animated');
-		}else{
+		} else {
 			$('.back-to-top').removeClass('show-btn');
 			$('.logo-header').removeClass('sticky-top');
 			$('.logo-header').removeClass('tada animated');
 		}
 	});
 
-	$('.back-to-top').click(function(event) {
-		$('html,body').animate({scrollTop:0},800);
+	$('.back-to-top').click(function (event) {
+		$('html,body').animate({ scrollTop: 0 }, 800);
+	});
+
+	$(window).click(function () {
+		var x = document.getElementById("playAudio");
+		x.play();
 	});
 
 });
+
+function on() {
+	document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+	document.getElementById("overlay").style.display = "none";
+}
